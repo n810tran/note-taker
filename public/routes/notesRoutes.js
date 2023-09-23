4d9fd4b14a6fdc55dda07a0f070610af31f11dfb
n810tran <nathaniel.k.tran@gmail.com>
2023-09-23 12:03:37 -0700

    added uuid, started notes routes

--- public/routes/notesRoutes.js
+++ public/routes/notesRoutes.js
@@ -2,6 +2,15 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
+router.get('/notes', (req, res) => {
 
+});
 
+router.post('/notes', (req, res) => {
+
+});
+
+router.delete('/notes/:id', (req, res) => {
+
+});
 module.exports = router;
