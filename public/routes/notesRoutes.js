d144b95011bb066705d5e77565f4f42f36222d62
n810tran <nathaniel.k.tran@gmail.com>
2023-10-29 20:46:16 -0700

    saving work on notesroutes.js

--- public/routes/notesRoutes.js
+++ public/routes/notesRoutes.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid'); // Import uuidv4 from the 'uuid' module
 
 router.get('/notes', (req, res) => {
 
 });
 
 router.post('/notes', (req, res) => {
-
+ const newNote = req.body;
+ newNote.id = uuidv4(); // Generate a unique ID
+ fs.writeFileSync
 });
 
 router.delete('/notes/:id', (req, res) => {
 
 });
+
 module.exports = router;
